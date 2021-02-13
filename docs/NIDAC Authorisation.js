ad3c5b1dd4a9503a3a677ffa13d4f0df31a81c96
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-02-13 06:30:14 +0000

    BAU - initial scripts for auth login stub and create case

---
+++ docs/NIDAC Authorisation.js
@@ -0,0 +1,36 @@
+// ==UserScript==
+// @name     NIDAC Authorisation
+// @namespace  http://tampermonkey.net/
+// @version   0.1
+// @description Authenticates for NIDAC
+// @author    You
+// @match     http*://*/auth-login-stub/gg-sign-in?continue=*national-import-duty-adjustment-centre*
+// @grant     none
+// @updateURL https://raw.githubusercontent.com/hmrc/national-import-duty-adjustment-centre-frontend/BAU-tamper-monkey-scripts/docs/NIDAC%20Authorisation.js
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    document.getElementsByName("redirectionUrl")[0].value = getBaseUrl() + "/national-import-duty-adjustment-centre";
+
+
+    document.getElementById('global-header').appendChild(createQuickButton())
+
+})();
+
+function createQuickButton() {
+    let button = document.createElement('button');
+    button.id = "quickSubmit";
+    button.innerHTML = 'Quick Submit';
+    button.onclick = () => document.getElementsByClassName('button')[0].click();
+    return button;
+}
+
+function getBaseUrl() {
+    let host = window.location.host;
+    if (window.location.hostname === 'localhost') {
+        host = 'localhost:8490'
+    }
+    return window.location.protocol + "//" + host;
+}
