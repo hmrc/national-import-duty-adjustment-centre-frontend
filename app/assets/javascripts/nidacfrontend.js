2395a57b822f204d771dd952056e40ec6684c739
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-04-15 08:01:46 +0100

    NF-404 - move js to external file

--- app/assets/javascripts/nidacfrontend.js
+++ app/assets/javascripts/nidacfrontend.js
@@ -10,4 +10,15 @@ if (window.history && window.history.replaceState && typeof window.history.repla
   window.history.replaceState(null, null, window.location.href);
 }
 
-
+// automatically submit document upload on file select
+function onFileSelect() {
+  try{
+    // NF-403 - hide the form controls, display the spinner and disable the 'Continue' button before submitting
+    document.getElementById("upload-form").className = "govuk-!-display-none";
+    document.getElementsByClassName("hidden-spinner-row").item(0).className = "govuk-summary-list__row";
+    document.getElementById("nidac-continue").disabled = true;
+  }
+  finally {
+    document.getElementById("upload-form").submit();
+  }
+}
