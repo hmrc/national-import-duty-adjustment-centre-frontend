57de4fc99597024dec15ba592397694db4f27909
Neil Moore <44603901+njmoorewitchcat@users.noreply.github.com>
2021-04-29 09:34:42 +0100

    NF-459 Fix Keepalive Url addition slash (#193)

--- app/uk/gov/hmrc/nationalimportdutyadjustmentcentrefrontend/config/AppConfig.scala
+++ app/uk/gov/hmrc/nationalimportdutyadjustmentcentrefrontend/config/AppConfig.scala
@@ -85,7 +85,7 @@ class AppConfig @Inject() (
   val addressLookupInitUrl: String         = s"$addressLookupBaseUrl${servicesConfig("address-lookup-frontend.init")}"
   val addressLookupConfirmedUrl: String    = s"$addressLookupBaseUrl${servicesConfig("address-lookup-frontend.confirmed")}"
 
-  val keepAliveUrl: String = s"$loginContinueUrl/${controllers.routes.KeepAliveController.keepAlive().url}"
+  val keepAliveUrl: String = s"$loginContinueUrl${controllers.routes.KeepAliveController.keepAlive().url}"
 
   val yourAddressLookupCallbackUrl: String =
     s"$loginContinueUrl/create${controllers.makeclaim.routes.AddressController.onUpdate("").url}"
