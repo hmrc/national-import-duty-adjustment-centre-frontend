269c4628c6034ff8256bf7041c4ef95824315643
Steve Sugden <46934286+SteveSugden@users.noreply.github.com>
2021-05-20 14:32:17 +0100

    NF-275 - fix for file upload problem in IE11

--- app/assets/javascripts/nidacfrontend.js
+++ app/assets/javascripts/nidacfrontend.js
@@ -1,10 +1,4 @@
 
-// =====================================================
-// Back link mimics browser back functionality
-// =====================================================
-// store referrer value to cater for IE - https://developer.microsoft.com/en-us/microsoft-edge/platform/issues/10474810/  */
-const docReferrer = document.referrer
-
 // prevent resubmit warning
 if (window.history && window.history.replaceState && typeof window.history.replaceState === 'function') {
   window.history.replaceState(null, null, window.location.href);
@@ -25,6 +19,6 @@ function onFileSelect() {
   finally {
     // timeout before submitting is to give Safari time to render un-hidden element
     // animations stop in Safari once form is submitted so animation start is delayed by 500 ms
-    setTimeout(() => {  document.getElementById("upload-form").submit(); }, 500);
+    setTimeout(function () {  document.getElementById("upload-form").submit(); }, 500);
   }
 }
